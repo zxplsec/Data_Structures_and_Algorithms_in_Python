415b12859178fc8fe230e41734b5cb9f8ce4d420
zxplsec <xpzhang.math@whu.edu.cn>
2017-09-01 14:20:34 +0800

    Update

--- slide1/slide1.sh
+++ slide1/slide1.sh
@@ -2,11 +2,14 @@
 
 xelatex slide1.tex
 rm -f slide1.ps  slide1.log slide1.aux slide1.out slide1.dvi slide1.bbl slide1.blg
-mupdf slide1.pdf &
+
 cd ..
 git add -A .
 git commit -m "Update"
 cd slide1
+
+mupdf slide1.pdf &
+
 #mkdir -p /tmp/workspace
 #cp -r * /tmp/workspace/
 #git add -A .

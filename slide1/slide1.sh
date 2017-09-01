77fbda541d6922e45a43331533c310bb7a244ace
zxplsec <xpzhang.math@whu.edu.cn>
2017-09-01 14:19:16 +0800

    Update

--- slide1/slide1.sh
+++ slide1/slide1.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-jupyter-nbconvert --to slides slides.ipynb --reveal-prefix=reveal.js
-mv slides.slides.html  index.html
+xelatex slide1.tex
+rm -f slide1.ps  slide1.log slide1.aux slide1.out slide1.dvi slide1.bbl slide1.blg
+mupdf slide1.pdf &
+cd ..
+git add -A .
+git commit -m "Update"
+cd slide1
 #mkdir -p /tmp/workspace
 #cp -r * /tmp/workspace/
 #git add -A .

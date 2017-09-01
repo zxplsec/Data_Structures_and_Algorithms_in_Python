e35e5fd8e81da8f15bdaf46283f9aceeadca7dfc
zxplsec <xpzhang.math@whu.edu.cn>
2017-09-01 14:26:22 +0800

    Update

---
+++ slide1/lec1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+xelatex lec1.tex
+rm -f lec1.nav lec1.snm lec1.toc lec1.vrb lec1.aux lec1.log lec1.out
+
+cd ..
+git add -A .
+git commit -m "Update"
+git push origin master
+cd lec1
+
+mupdf lec1.pdf &
+
+#mkdir -p /tmp/workspace
+#cp -r * /tmp/workspace/
+#git add -A .
+#git commit -m "Update"
+#git checkout -B gh-pages
+#cp -r /tmp/workspace/* .
+#git add -A .
+#git commit -m "new version"
+#git push origin master gh-pages
+#git checkout master
+#rm -rf /tmp/workspace

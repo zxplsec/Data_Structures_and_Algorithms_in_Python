41d321398c019908d638a4f0a5b318dc2b2be465
zxplsec <xpzhang.math@whu.edu.cn>
2017-10-12 21:36:51 +0800

    Update

--- ch02/lec.sh
+++ ch02/lec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 xelatex lec.tex
-rm -f lec.nav lec.snm lec.toc lec.vrb lec.aux lec.log lec.out
-#mupdf lec.pdf &
-cp lec.pdf ../lec1.pdf
+rm -f *.nav *.snm *.toc *.vrb *.aux *.log *.out
+#mupdf *.pdf &
+cp lec.pdf ../lec2.pdf
